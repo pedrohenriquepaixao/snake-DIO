994001f8b86f52230a2eea51e51637d55a365fed
pedrohenriquepaixao <pedrohenriquedapaixao@outlook.com>
2021-05-13 14:08:05 -0300

    Criar Snake

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 let canvas = document.querySelector("#snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG()
 {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "#FF0000";
     context.fillRect(0,0, 16 * box, 16 * box );
 }
 
+function criarSnake()
+{
+    for(i = 0; i < snake.length ; i++)
+    {
+        context.fillStyle =  "black";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarSnake();
